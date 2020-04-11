b434bb15a454d66527bd84f1460f968da0356613
HarrasIntissar <intissar17harras@gmail.com>
2020-04-11 19:51:03 +0100

    adding attribute to CV classe

--- Gestion_Des_Formations/src/main/java/csi/master/gestion_des_formations/entities/CurriculumVitae.java
+++ Gestion_Des_Formations/src/main/java/csi/master/gestion_des_formations/entities/CurriculumVitae.java
@@ -1,9 +1,12 @@
 package csi.master.gestion_des_formations.entities;
 
+import java.util.Calendar;
+
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.validation.constraints.Email;
 
 import lombok.Data;
 
@@ -21,5 +24,19 @@ public class CurriculumVitae {
 	private Long id_CV;
 	
 	private Long formateurId;
-
+	
+	private String nomComplet;
+	private String metier;
+	private String phone;
+	@Email
+	private String email;
+	
+	private Calendar dateDebutFormation;
+	private Calendar dateFinFormation;
+	private String etablissement;
+	private String villeFormation;
+	
+	private Calendar dateDebutExperience;
+	private Calendar dateFinExperience;
+	private String villeExperience;
 }
