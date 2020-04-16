dcd857d537f20917a0d3771501a73df522cdec53
HarrasIntissar <intissar17harras@gmail.com>
2020-04-16 14:37:54 +0100

    just added

--- GestionDesFormationsWEB/src/app/services/Authentication.service.ts
+++ GestionDesFormationsWEB/src/app/services/Authentication.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Router } from '@angular/router';
-import { User } from '../_model/user';
+import { User } from '../_models/user';
 
 @Injectable({
   providedIn: 'root'
